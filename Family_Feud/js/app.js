740dfacf7b82ad23a8086526d535cf1d757b1175
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-09-24 14:15:00 -0400

    padded and positioned tile buttons. Need to figure out anchoring system

--- Family_Feud/js/app.js
+++ Family_Feud/js/app.js
@@ -6,8 +6,23 @@ $(() => {
 
 $('.tile1').on('click', () => {
   console.log('yeah baby');
-})
+});
 
+$('.tile2').on('click', () => {
+  console.log('yeah baby1');
+});
+
+$('.tile3').on('click', () => {
+  console.log('yeah baby2');
+});
+
+$('.tile4').on('click', () => {
+  console.log('yeah baby3');
+});
+
+$('.tile5').on('click', () => {
+  console.log('yeah baby4');
+});
 
 
 
