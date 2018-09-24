4e41e92ee9e400b8794668cf817c609a1231ca8c
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-09-24 16:25:29 -0400

    styling submit button

--- Family_Feud/js/app.js
+++ Family_Feud/js/app.js
@@ -1,28 +1,10 @@
-console.log('this is app.js');
+// console.log('this is app.js');
 
 
 $(() => {
 
 
-$('.tile1').on('click', () => {
-  console.log('yeah baby');
-});
 
-$('.tile2').on('click', () => {
-  console.log('yeah baby1');
-});
-
-$('.tile3').on('click', () => {
-  console.log('yeah baby2');
-});
-
-$('.tile4').on('click', () => {
-  console.log('yeah baby3');
-});
-
-$('.tile5').on('click', () => {
-  console.log('yeah baby4');
-});
 
 
 
