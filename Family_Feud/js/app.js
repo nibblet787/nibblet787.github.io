1b7a083ec06aa5ae75860ccb1f7acfdddbfba8ed
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-09-25 01:20:28 -0400

    finished gui. Now for functionality

--- Family_Feud/js/app.js
+++ Family_Feud/js/app.js
@@ -1,38 +1,55 @@
 // console.log('this is app.js');
 
 
-$(() => {
+// $(() => {
 
 
-$('.survey-says').on('click', input-box);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// $('.survey-says').on('click', ()=> {
+//   $('.tile5').stop().animate(
+//   {rotation: 720},
+//   {
+//     duration: 1000,
+//     step: function(now, fx) {
+//       $(this).css({"transform": "rotatex("+now+"deg)"}, {"transition-timing-function": "easeOutCirc"}, {"visibility": "hidden"});
+//       $(this).css('visibility', 'hidden');
+//     }
+//   }
+// );
+$('.survey-says').on('click', ()=> {
+  $('.tile5').attr('id', 'spin');
+  $dark();
 });
+
+const $dark = () => {
+  $('.tile5').css('visibility', 'hidden');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// });
