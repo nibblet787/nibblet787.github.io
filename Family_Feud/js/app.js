83d8ae9c4777d479e1467a8e9838f67ea5f5ee4a
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-09-26 23:54:36 -0400

    added more potential rounds and randomized their order

--- Family_Feud/js/app.js
+++ Family_Feud/js/app.js
@@ -1,43 +1,7 @@
-// console.log('this is app.js');
-
-
-
-
-// $(() => {
-
-
-// const challenges = [{
-//                     challenge: "Name something you operate by pushing a button",
-//                     answers: [["Elevator",37], ["Telephone",13], ["Microwave",10], ["TV",8], ["Doorbell",6]
-//                   ]},
-                    // {challenge: "What's the first thing you do after waking up from a nap?",
-                    // answers: [["Go To Bathroom",21], ["Check The Time",18], ["Stretch",15], ["Yawn",9], ["Get A Drink",8]]},
-//                     {challenge: "Name something people run out of", answers: [["Milk",26], ["Money",22], ["Gas",16], ["Sugar",11], ["Toilet Paper",10]]
-//                   },
-//                   {challenge: "Name something that puts people to sleep", answers: [["Medication",22],["Reading",16],["Music",14],["TV",12],["Milk",8]]
-//                 }];
-// "Name something a man has to force himself to do daily":[["Get Up",31],["Shave",16],["Go To Work",14],["Exercise",10],["Clean",9],["Brush Teeth",6]],
-// "Tell us something a mother says to her child over and over again":[["No",30],["Don't",15],["I Love You",10],["Be Quiet",8],["Clean Your Room",7],["Don't Touch",7],["Brush Teeth",3],["Wash Hand",2]],
-// "Name something in the house that is very difficult to move":[["Refrigerator",42],["Piano",14],["Couch",12],["Bed",9],["Dresser",5],["TV",5],["Stove",3]],
-// "Name something you put a dust cover over":[["VCR",31],["Typewriter",18],["Toaster",12],["Car",9],["Stereo",7],["Couch",4],["Chair",3],["Computer",3]]]
-// console.log(challenges[3].answers[1][0]);
-
-// for (i = 0; i < challenges.length; i++) {
-// let test = Math.floor(Math.random * challenges.length -1)[i];
-// console.log(test.challenge);
-// // let randomChallenges =
-// };
-
-
 
 
 // =============================================================================
 // Global variable build =================================
-// let $player1Bank = $('.currentPlayer').text();
-// $player1Bank = $parseInt();
-// console.log($player1Bank);
-let $player2Bank = $('.currentPlayer').text();
-// console.log(typeof $player2Bank);
 let $bank = 50;
 let $question;
 let $answer0;
@@ -55,10 +19,35 @@ let $money4;
 const challenges = [{
                   challenge: "Name something you operate by pushing a button",
                   answers: [["Elevator", 37], ["Telephone", 13], ["Microwave", 10], ["TV", 8], ["Doorbell", 6]]},
-                  {
+                {
                   challenge: "What's the first thing you do after waking up from a nap?",
                   answers: [["Go To The Bathroom", 21], ["Check The Time", 18], ["Stretch", 15], ["Yawn", 9], ["Get A Drink", 8]]
-                }];
+                },
+                {
+                  challenge: "Name something people run out of",
+                  answers: [["Milk", 26], ["Money", 22], ["Gas", 16], ["Sugar", 11], ["Toilet Paper", 10]]
+                },
+                {
+                  challenge: "Name something that puts people to sleep",
+                  answers: [["Medication", 22], ["Reading", 16], ["Music", 14], ["TV", 12], ["Milk", 8]]
+                },
+                {
+                  challenge: "Name something a person has to force themselves to do daily",
+                  answers: [["Get Up", 31], ["Shave", 16], ["Go To Work", 14], ["Exercise", 10], ["Clean", 9]]
+                },
+                {
+                  challenges: "Tell us something a mother says to her child over and over again",
+                  answers: [["No", 30], ["Don't", 15], ["I Love You", 10], ["Be Quiet", 8], ["Clean Your Room", 7]]
+                },
+                {
+                  challenges: "Name something in the house that is very difficult to move",
+                  answers: [["Refrigerator", 42], ["Piano", 14], ["Couch", 12], ["Bed", 9], ["Dresser",5]]
+                },
+                {
+                  challenges: "Name something you put a dust cover over"
+                  answers: [["Blu-ray player", 31], ["Typewriter", 18], ["Toaster", 12], ["Car", 9], ["Stereo",7]]
+                }
+              ];
 
 // ================================= ROUND 1 VARIABLE SETTINGS =================================
 const $round1 = () => {
@@ -74,7 +63,6 @@ const $round1 = () => {
               $answer4 = challenges[0].answers[4][0];
               $money4 = challenges[0].answers[4][1];
 $stringBuild();
-console.log($money3);
 };
 
 // ================================= ROUND 2 VARIABLE SETTINGS =================================
@@ -93,6 +81,102 @@ const $round2 = () => {
 $stringBuild();
 };
 
+// ================================= ROUND 3 VARIABLE SETTINGS ================================
+const $round3 = () => {
+              $question = challenges[2].challenge;
+              $answer0 = challenges[2].answers[0][0];
+              $money0 = challenges[2].answers[0][1];
+              $answer1 = challenges[2].answers[1][0];
+              $money1 = challenges[2].answers[1][1];
+              $answer2 = challenges[2].answers[2][0];
+              $money2 = challenges[2].answers[2][1];
+              $answer3 = challenges[2].answers[3][0];
+              $money3 = challenges[2].answers[3][1];
+              $answer4 = challenges[2].answers[4][0];
+              $money4 = challenges[2].answers[4][1];
+$stringBuild();
+};
+
+// ================================= ROUND 4 VARIABLE SETTINGS ================================
+const $round4 = () => {
+              $question = challenges[3].challenge;
+              $answer0 = challenges[3].answers[0][0];
+              $money0 = challenges[3].answers[0][1];
+              $answer1 = challenges[3].answers[1][0];
+              $money1 = challenges[3].answers[1][1];
+              $answer2 = challenges[3].answers[2][0];
+              $money2 = challenges[3].answers[2][1];
+              $answer3 = challenges[3].answers[3][0];
+              $money3 = challenges[3].answers[3][1];
+              $answer4 = challenges[3].answers[4][0];
+              $money4 = challenges[3].answers[4][1];
+$stringBuild();
+};
+
+// ================================= ROUND 5 VARIABLE SETTINGS ================================
+const $round5 = () => {
+              $question = challenges[4].challenge;
+              $answer0 = challenges[4].answers[0][0];
+              $money0 = challenges[4].answers[0][1];
+              $answer1 = challenges[4].answers[1][0];
+              $money1 = challenges[4].answers[1][1];
+              $answer2 = challenges[4].answers[2][0];
+              $money2 = challenges[4].answers[2][1];
+              $answer3 = challenges[4].answers[3][0];
+              $money3 = challenges[4].answers[3][1];
+              $answer4 = challenges[4].answers[4][0];
+              $money4 = challenges[4].answers[4][1];
+$stringBuild();
+};
+
+// ================================= ROUND 6 VARIABLE SETTINGS ================================
+const $round6 = () => {
+              $question = challenges[5].challenge;
+              $answer0 = challenges[5].answers[0][0];
+              $money0 = challenges[5].answers[0][1];
+              $answer1 = challenges[5].answers[1][0];
+              $money1 = challenges[5].answers[1][1];
+              $answer2 = challenges[5].answers[2][0];
+              $money2 = challenges[5].answers[2][1];
+              $answer3 = challenges[5].answers[3][0];
+              $money3 = challenges[5].answers[3][1];
+              $answer4 = challenges[5].answers[4][0];
+              $money4 = challenges[5].answers[4][1];
+$stringBuild();
+};
+
+// ================================= ROUND 7 VARIABLE SETTINGS ================================
+const $round7 = () => {
+              $question = challenges[6].challenge;
+              $answer0 = challenges[6].answers[0][0];
+              $money0 = challenges[6].answers[0][1];
+              $answer1 = challenges[6].answers[1][0];
+              $money1 = challenges[6].answers[1][1];
+              $answer2 = challenges[6].answers[2][0];
+              $money2 = challenges[6].answers[2][1];
+              $answer3 = challenges[6].answers[3][0];
+              $money3 = challenges[6].answers[3][1];
+              $answer4 = challenges[6].answers[4][0];
+              $money4 = challenges[6].answers[4][1];
+$stringBuild();
+};
+
+// ================================= ROUND 8 VARIABLE SETTINGS ================================
+const $round8 = () => {
+              $question = challenges[7].challenge;
+              $answer0 = challenges[7].answers[0][0];
+              $money0 = challenges[7].answers[0][1];
+              $answer1 = challenges[7].answers[1][0];
+              $money1 = challenges[7].answers[1][1];
+              $answer2 = challenges[7].answers[2][0];
+              $money2 = challenges[7].answers[2][1];
+              $answer3 = challenges[7].answers[3][0];
+              $money3 = challenges[7].answers[3][1];
+              $answer4 = challenges[7].answers[4][0];
+              $money4 = challenges[7].answers[4][1];
+$stringBuild();
+};
+
 // Constructing the string for display on the game board =================================
 const $stringBuild = () => {
                   $('.question').text($question);
@@ -101,6 +185,27 @@ const $stringBuild = () => {
                   $('#answer2').text($answer2 + " $" + $money2);
                   $('#answer3').text($answer3 + " $" + $money3);
                   $('#answer4').text($answer4 + " $" + $money4);
+};
+
+// Randomizer for game rounds, both starting and in-game ==============================
+const $rando = () => {
+  let gamePicker = Math.ceil(Math.random() * 7);
+  console.log(gamePicker);
+  if (gamePicker = 1) {
+    $round1();
+  } else if (gamePicker = 2) {
+    $round2();
+  } else if (gamePicker = 3) {
+    $round3();
+  } else if (gamePicker = 4) {
+    $round4();
+  } else if (gamePicker = 5) {
+    $round5();
+  } else if (gamePicker = 6) {
+    $round6();
+  } else {
+    $round7();
+  }
 };
 
 // Restting game tiles to default setting and attributes for the next round ==================
@@ -124,12 +229,13 @@ const winner = () => {
   }
 };
 
+
 // Check after each successful guess to see if all tiles have been cleared =================
 const $checkAllTiles = () => {
   if ($bank < 99 && $('.tile0').attr('id') === 'spin' && $('.tile1').attr('id') === 'spin' && $('.tile2').attr('id') === 'spin' && $('.tile3').attr('id') === 'spin' && $('.tile4').attr('id') === 'spin') {
     alert('The board has been cleared. Moving on to the next round!');
     $resetTiles();
-    $round2();
+    $round8();
   } else {
     console.log('checked all tiles');
   }
@@ -140,11 +246,9 @@ const $playerSwitch = () => {
   const $player1 = $('#Player1')
   const $player2 = $('#Player2')
   if ($player1.attr('class') === 'currentPlayer') {
-            console.log($('.currentPlayer').attr('id'));
             $player1.removeClass('currentPlayer').addClass('other').css('color', 'white');
             $player2.removeClass().addClass('currentPlayer').css('color', 'cyan');
   } else if ($player2.attr('class') === 'currentPlayer') {
-            console.log($('.currentPlayer').attr('id'));
             $player2.removeClass('currentPlayer').addClass('other').css('color', 'white');
             $player1.removeClass().addClass('currentPlayer').css('color', 'cyan');
   } else {
@@ -251,8 +355,8 @@ $('.survey-says').on('click', (e)=> {
 });
 
 // Game Start =========================
-$round1();
-
+// $round1();
+$rando();
 
 // .fadeOut(1000)
 
