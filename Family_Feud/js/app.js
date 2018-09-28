6ebf51a5d4f70632ae762bfd0b32b01f5eb9c117
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-09-28 10:46:30 -0400

    Tweaked reset code

--- Family_Feud/js/app.js
+++ Family_Feud/js/app.js
@@ -225,6 +225,7 @@ const winner = () => {
     } else if ($bank <= 0) {
       $('.tile0, .tile1, .tile2, .tile3, .tile4').attr('id', 'spin');
       $('.question').html($currentPlayer + ' ran out of money! THEY LOSE!!<br/>Click \'RESET\' to play again.').removeClass().addClass('no-money');
+      $('.reset').removeClass().addClass('clickReset');
       $('.survey-says').off(); // Prevents player from continuing to play without resetting
       // alert($('.currentPlayer').attr('id') + ' ran out of money! THEY LOSE!! Resetting board...');
       $('.currentPlayer').text("$" + 0);
